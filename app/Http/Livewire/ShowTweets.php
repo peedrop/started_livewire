c9f26e27cdccdc0786d8711ae92a4efed8b911d6
Pedro Paulo <pedropaulosilvaf@gmail.com>
2022-02-17 17:25:11 -0300

    like and dislike a tweet

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -34,4 +34,16 @@ class ShowTweets extends Component
 
         $this->content = '';
     }
+
+    public function like($tweet_id) {
+        $tweet = Tweet::find($tweet_id);
+
+        $tweet->likes()->create([
+            'user_id' => auth()->user()->id
+        ]);
+    }
+
+    public function unlike(Tweet $tweet) {
+        $tweet->likes()->delete();
+    }
 }
